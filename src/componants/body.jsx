501b9758b6529a5946cdd3e66442e27a559e4579
Infant-Ryan <ini0k17@gmail.com>
2025-01-10 09:34:24 +0530

    chore

--- src/componants/body.jsx
+++ src/componants/body.jsx
@@ -20,7 +20,7 @@ export default function Body() {
       <div className="flex justify-center"  >
         <p className=" text-9xl font-bold">{Num}</p>
       </div>
-      <div className="flex justify-center gap-4">
+      <div className="flex flex-col justify-center gap-4 md:flex-row md:justify-center ">
         <button onClick={increment} className="bg-black text-white hover:bg-white hover:text-black border-2 border-black rounded-md   h-8 w-16 ">++</button>
         <button onClick={decrement} className="bg-black text-white hover:bg-white hover:text-black border-2 border-black rounded-md   h-8 w-16 ">--</button>
         <button onClick={reset} className="bg-black text-white hover:bg-white hover:text-black border-2 border-black rounded-md   h-8 w-16 ">Reset</button>
